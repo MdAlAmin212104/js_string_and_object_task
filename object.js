5f93c93485c3e1a48b91fffb54a0f6bbf80a2e13
MdAlAmin212104 <mdalamin212104@gmail.com>
2024-02-01 20:00:28 +0600

    fineal problem solved

--- object.js
+++ object.js
@@ -71,3 +71,41 @@ console.log(countOfProperty);
 
 //**Output:**
  //4
+
+
+
+  /*
+ ### Task-5 (Hard)
+
+Loop through an object and print the key-value pairs with their `types`
+
+*/
+
+// **Input:**
+
+let myObject = {
+
+    name: 'John Doe',
+    age: 25,
+    city: 'Example City',
+    isStudent: true
+
+};
+
+for(let key in myObject){
+      const value = myObject[key];
+      const type = typeof value;
+      console.log(`key : ${key} | type : ${type}`);
+}
+
+/*
+**Output:**
+
+<br>
+
+    key: name | type:  string
+    key: age | type:  number
+    key: city | type:  string
+    key: isStudent | type:  boolean
+
+*/
