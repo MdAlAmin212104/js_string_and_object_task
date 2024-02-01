9adfd2fa9d4c43f7cf2a387b697b220201bdf79d
MdAlAmin212104 <mdalamin212104@gmail.com>
2024-02-01 19:58:16 +0600

    object problem solved

---
+++ object.js
@@ -0,0 +1,31 @@
+/*### Task-1
+Access the `golden rod` color value in output.
+
+*/
+const colors = {
+    red: "#ff0000",
+    green: "#00ff00",
+    blue: "#0000ff",
+    "golden rod": '#daa520'
+};
+
+const goldenColor = colors['golden rod']
+console.log(goldenColor);
+
+/*
+### Task-2
+For this object below add a property named `passenger capacity` with value 5
+
+
+*/
+
+/* */
+const car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020,
+    "passenger capacity": 5,
+};
+
+const passenger = car["passenger capacity"];
+console.log(passenger);
