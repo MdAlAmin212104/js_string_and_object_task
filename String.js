e60399087cc8eede11fbadc66cbdab7b0caebf4e
MdAlAmin212104 <mdalamin212104@gmail.com>
2024-02-01 19:57:02 +0600

    quation add but solution not added

--- String.js
+++ String.js
@@ -47,3 +47,15 @@ for (let i = 0; i<textFormated1.length; i++){
       
 }
 
+
+/* 
+### Task-4: 
+If a given string has either x, replace x by y. if the given string has X, replace it by Y.
+
+    Hint: You should be able to check whether x or X exists. After that, search online how to replace a character in a string.
+
+---
+### Task-5:
+Capitalize Every first Letter of each word in a String
+
+*/
