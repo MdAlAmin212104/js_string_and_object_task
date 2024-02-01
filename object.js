ee44074cba1b944ada1fdac1c551483330fc1712
MdAlAmin212104 <mdalamin212104@gmail.com>
2024-02-01 19:58:54 +0600

    task 3 added

--- object.js
+++ object.js
@@ -29,3 +29,21 @@ const car = {
 
 const passenger = car["passenger capacity"];
 console.log(passenger);
+
+/* 
+### Task-3
+Display the physics marks as output.
+```js
+*/
+
+const student = {
+    name: "Hamim Sakep",
+    id: 5421,
+    physics: {
+        subject: "HSC Physics",
+        author: "Shahjahan Tapan",
+        marks: 30
+    }
+};
+
+console.log(student.physics.marks);
